49ca5a1bcd4cd224087f063b95f975ba4c674eaf
Raphael Ndauwa Karanja <rkaranja@cytonn.com>
2018-10-31 12:31:00 +0300

    Add domenstration for prototypes

--- js/js_objects.js
+++ js/js_objects.js
@@ -3,6 +3,7 @@
 //Object Functions
 //Accessing values in an object
 //Assigning new object properties and values
+//Prototypes
 
 let person = {
     name: 'Frank',
@@ -25,3 +26,24 @@ person.moreInformation = function () {
 };
 
 console.log(person.moreInformation());
+
+
+//prototypes
+function someone(name,age, height) {
+    this.name = name;
+    this.age = age;
+    this.height = height;
+    this.getInformation = function () {
+        return this.name + ' is ' + this.age + ' years old and is ' + this.height + ' feet tall.';
+    }
+}
+
+let someperson = new someone('Frank', 28, 6);
+let someperson2 = new someone('Ian', 25, 7);
+let someperson3 = new someone('Raphael', 24, 5.5);
+let someperson4 = new someone('William', 35, 5.9);
+
+document.getElementById('someone').innerText =someperson.getInformation();
+document.getElementById('someone2').innerText =someperson2.getInformation();
+document.getElementById('someone3').innerText =someperson3.getInformation();
+document.getElementById('someone4').innerText =someperson4.getInformation();
