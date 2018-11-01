b3feef85dbfdfb1a047a908d485a5c932b589977
Raphael Ndauwa Karanja <rkaranja@cytonn.com>
2018-11-01 12:37:41 +0300

    display arrays

---
+++ js/arrays.js
@@ -0,0 +1,20 @@
+// let students = ['Ayumba', 'Frank', 'Ian', 'Bernard', 'Joan', 'Salome', 'Eneki', 'Diana', 'Phillip'];
+// students.push('Charles');
+// students.splice(9, 1);
+
+let students = [
+    {
+        name: 'Ayumba',
+        height: 6
+    },
+    {
+        name: 'Frank',
+        height: 5.9
+    },
+    {
+        name: 'Ian',
+        height: 6.1
+    },
+];
+
+console.log(students[0].name);
